2f70f17d5789ce41a4d79f611fae5285a5d7bf0f
naongagonwe <naongagondwe3@gmail.com>
2026-07-19 23:51:17 +0200

    Update app version to 1.2.0 in settings and pubspec

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -410,7 +410,7 @@ class _SettingsPageState extends State<SettingsPage> {
                   ListTile(
                     leading: Icon(Icons.info_outline, color: Color(0xFF64B5F6)),
                     title: Text('Loans Tracker'),
-                    subtitle: Text('Version: beta 1'),
+                    subtitle: Text('Version: 1.2.0'),
                   ),
                   ListTile(
                     leading: Icon(Icons.person, color: Color(0xFF64B5F6)),
